8a797f44d88edf993eaefa4c593440f9d206c8fc
BrianCKWang <brian.ck.wang@gmail.com>
2021-01-14 18:07:30 -0500

    finish api functions and search history

--- assets/js/mainpage.js
+++ assets/js/mainpage.js
@@ -1,96 +1,154 @@
-var mainData;
+var cityName = "Chicago";
+var apiKey = "2e445b75f57f2c3f031b78969d6f739f";
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#cityname");
+var infoSectionEl = document.querySelector("#info-section");
 var currentDayCardEl = document.querySelector("#current-day-card");
-var forecastDay1El = document.querySelector("#day-1");
-var forecastDay2El = document.querySelector("#day-2");
-var forecastDay3El = document.querySelector("#day-3");
-var forecastDay4El = document.querySelector("#day-4");
-var forecastDay5El = document.querySelector("#day-5");
+var searchHistoryEl = document.querySelector("#search-history");
+var searchHistory = [];
+var maxSearchHistory = 8;
+var localStorageName = "brianckwang-weather-dashboard-items";
+var firstPass = false;
 
-var getCurrentDay_Main = function(apiUrl) {
-  // make a request to the url
-  fetch(apiUrl)
-  .then(function(response) {
-    // request was successful
-    if (response.ok) {
-      response.json().then(function(data) {
-        displayDay_main(data);
-        getCurrentDay_UV("http://api.openweathermap.org/data/2.5/uvi?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&appid=" + apiKey);
-      });
-    } else {
-      alert("Error: " + response.statusText);
-    }
-  })
-  .catch(function(error) {
-    // Notice this `.catch()` getting chained onto the end of the `.then()` method
-    alert("Unable to connect to OpenWeather");
-    
-  });
+var loadSearchHistory = function() {
+  searchHistory = JSON.parse(localStorage.getItem(localStorageName));
+
+  // if nothing in localStorage, create a new object to track all task status arrays
+  if (!searchHistory) {
+    searchHistory = [];
+  }
+  else{
+    searchHistory.forEach(function(item, index){
+      // console.log(item);
+      $(searchHistoryEl).append('<li class="list-group-item">' + item + '</li>');
+    })
+  }
 };
 
-var getCurrentDay_UV = function(apiUrl) {
-  // make a request to the url
-  fetch(apiUrl)
-  .then(function(response) {
-    // request was successful
-    if (response.ok) {
-      response.json().then(function(data) {
-        displayDay_UV(data);
-      });
-    } else {
-      alert("Error: " + response.statusText);
-    }
-  })
-  .catch(function(error) {
-    // Notice this `.catch()` getting chained onto the end of the `.then()` method
-    alert("Unable to connect to OpenWeather");
-    
-  });
+var saveSearchHistory = function() {
+  localStorage.setItem(localStorageName, JSON.stringify(searchHistory));
 };
 
-var displayDay_main = function(data) {
-  console.log(data);
-  // console.log(data.name);
-  // console.log(data.main.temp);
-  // console.log(data.main.humidity);
-  // console.log(data.wind.speed);
-  // console.log(data.main.temp);
-  // console.log(data.coord.lat);
-  // console.log(data.coord.lon);
-
-  mainData = data;
-
-  // currentDayCardEl.querySelector(".temp").textContent = "Temp: " + mainData.main.temp + "F";
-  // currentDayCardEl.querySelector(".humidity").textContent = "Humidity: " + mainData.main.humidity + "%";
-  // currentDayCardEl.querySelector(".wind").textContent = "Temp: " + mainData.wind.speed + "meter/sec";
-  $(currentDayCardEl).find(".date span").text(data.name + " (" + moment().format('ddd, YYYY/MM/DD') + ")");
-  $(currentDayCardEl).find(".temp span").text(mainData.main.temp);
-  $(currentDayCardEl).find(".humidity span").text(mainData.main.humidity );
-  $(currentDayCardEl).find(".wind span").text(mainData.wind.speed);
-  
+var updateSearchHistory = function() {
+  searchHistory.unshift(cityName);
+  $(searchHistoryEl).prepend('<li class="list-group-item">' + cityName + '</li>');
+  if(searchHistory.length > maxSearchHistory){
+    searchHistory.pop();
+    $(searchHistoryEl).children().last().remove();
+  }
+  saveSearchHistory();
 }
 
-var displayDay_UV = function(data) {
-  console.log(data);
-  // console.log(data.lat);
-  // console.log(data.lon);
-  // console.log(data.value);
-  mainData.UVIndex = data.value;
-  // console.log(mainData);
-  $(currentDayCardEl).find(".uv span").text(mainData.UVIndex);
+var  getCurrentDay_Main = function(cityName, apiKey) {
+  // make a request to the url
+
+  fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&appid=" + apiKey)
+  .then(function(response){
+    if(response.ok){
+      return response.json();
+    }
+    else{
+      console.log('Unable to connect to OpenWeather - Current day api');
+    }
+  })
+  .then(function(data) {
+    return fetch("http://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&exclude=minutely,hourly,alerts&units=metric&appid=" + apiKey);
+  })
+  .then(function(response) {
+    if(response.ok){
+      return response.json();
+    }
+    else{
+      console.log('Unable to connect to OpenWeather - One call api');
+    }
+  })
+  .then(function(data) {
+    displayWeatherInfo(data);
+    if(firstPass){
+      updateSearchHistory();
+    }
+    firstPass = true;
+    
+    $(infoSectionEl).css("visibility", "visible");
+    
+  })
+  .catch(function(error) {
+    // Notice this `.catch()` getting chained onto the end of the `.then()` method
+    console.log('Unable to connect to OpenWeather', error);
+  });
+
+};
+
+var displayDates = function() {
+  $(currentDayCardEl).find(".date span").text(cityName + " (" + moment().format('ddd, YYYY/MM/DD') + ")");
+  for(var i = 1; i <= 5; i++){
+    var forecastDayEl = document.querySelector("#day-" + i);
+    $(forecastDayEl).find(".date").text(moment().add(i, 'days').format('ddd, MM/DD'));
+  }
+}
+
+var displayWeatherInfo = function(data) {
+  // moment().unix(data.current.dt).format('ddd, YYYY/MM/DD')
+  // console.log(data);
+  $(currentDayCardEl).find(".date span").text(cityName.charAt(0).toUpperCase() + cityName.slice(1) + " (" + moment().format('ddd, YYYY/MM/DD') + ")");
+  $(currentDayCardEl).find(".date img").attr("src", "./assets/icons/" + data.current.weather[0].icon + ".png");
+  $(currentDayCardEl).find(".temp span").text(data.current.temp);
+  $(currentDayCardEl).find(".humidity span").text(data.current.humidity );
+  $(currentDayCardEl).find(".wind span").text(data.current.wind_speed);
+  
+  $(currentDayCardEl).find(".uv span").text(data.current.uvi);
   $(currentDayCardEl).find(".uv span").removeClass("badge-success badge-warning badge-danger");
-  if(mainData.UVIndex < 2){
+
+  if(data.current.uvi < 2){
     $(currentDayCardEl).find(".uv span").addClass("badge-success");
   }
-  else if(mainData.UVIndex < 5){
+  else if(data.current.uvi < 5){
     $(currentDayCardEl).find(".uv span").addClass("badge-warning");
   }
   else{
     $(currentDayCardEl).find(".uv span").addClass("badge-danger");
   }
+
+  for(var i = 1; i <= 5; i++){
+    var forecastDayEl = document.querySelector("#day-" + i);
+    // src="./assets/icons/01d.png"
+
+    // console.log("./assets/icons/" + data.daily[i].weather.icon + ".png");
+    // console.log(data.daily[i].weather[0].icon);
+
+    $(forecastDayEl).find(".icon img").attr("src", "./assets/icons/" + data.daily[i].weather[0].icon + ".png");
+    $(forecastDayEl).find(".temp span").text(data.daily[i].temp.day);
+    $(forecastDayEl).find(".humidity span").text(data.daily[i].humidity);
+  }
 }
 
-var cityName = "chicago";
-var apiKey = "2e445b75f57f2c3f031b78969d6f739f";
-var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
 
-getCurrentDay_Main(apiUrl);
+
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+  // get value from input element
+  if(cityName != nameInputEl.value.trim()){
+    cityName = nameInputEl.value.trim();
+
+    if (cityName) {
+      getCurrentDay_Main(cityName, apiKey);
+      nameInputEl.value = "";
+    } else {
+      alert("Please enter a city name");
+    }
+  }
+  nameInputEl.value = "";
+};
+
+displayDates();
+loadSearchHistory();
+if(searchHistory.length > 0){
+  cityName = searchHistory[0];
+  getCurrentDay_Main(searchHistory[0], apiKey);
+}
+
+
+// $(infoSectionEl).css("visibility", "visible");
+// getCurrentDay_Main(cityName, apiKey);
+
+userFormEl.addEventListener("submit", formSubmitHandler);
